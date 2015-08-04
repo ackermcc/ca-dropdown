6ee6c4b729ae2afc27f1f66637f50100c76f6c46
Chad Ackerman <ackermcc@gmail.com>
2015-08-03 21:15:19 -0700

    init

---
+++ js/ca-dropdown.js
@@ -0,0 +1,12 @@
+$(function(){
+
+	var menuWindow = $('.ca-menu-window');
+	var menuContainer = $('.ca-menus-container');
+	var menu = $('.ca-menu');
+
+	//Set container width to equal *2 the rendered window width;
+	alert(menuWindow.width() + ', ' + menu.width() + ', ' + menuContainer.width());
+
+	//Set menu width to equal the window frame width
+
+});

a4ac16e14b7bd606e9e351323899f5f0aea9c019
Chad Ackerman <chad.ackerman@nitelifecam.comgit config --global user.email ackermcc@gmail.com>
2015-08-03 22:47:26 -0700

    basic functionality is working

--- js/ca-dropdown.js
+++ js/ca-dropdown.js
@@ -1,12 +1,45 @@
-$(function(){
+$(document).ready(function(){
 
 	var menuWindow = $('.ca-menu-window');
 	var menuContainer = $('.ca-menus-container');
 	var menu = $('.ca-menu');
 
-	//Set container width to equal *2 the rendered window width;
-	alert(menuWindow.width() + ', ' + menu.width() + ', ' + menuContainer.width());
+	var childVisible = 0;
 
-	//Set menu width to equal the window frame width
+	// Dynamically set height...
+	function setMenuHeight(m){
+		menuWindow.height(m.height());
+	};
+
+	//Set height on select.
+	$('#jq-dropdown-1').on('show', function(event, dropdownData){
+		console.log(menu.first());
+		if (childVisible == 0) {
+			setMenuHeight(menu.first());
+		} else {
+			setMenuHeight(menu.last());
+		}
+	});
+
+	//Select option and slide menu
+	menu.find('li.group').click(function(){
+		menuContainer.animate({
+			left: -menu.width()
+		}, 350, function(){
+			childVisible = 1;
+			setMenuHeight(menu.last());
+		});
+	});
+
+	//Select title to return
+	menu.find('li.title').click(function(){
+		menuContainer.animate({
+			left: 0
+		}, 350, function(){
+			childVisible = 0;
+			setMenuHeight(menu.first());
+		});
+	});
+
 
 });
